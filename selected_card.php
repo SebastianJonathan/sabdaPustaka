7f0b94f440a2c3602769b7b4d204840489e9202d
sinyoh <hariyantowinston@gmail.com>
2023-07-17 16:15:17 +0700

    test

--- selected_card.php
+++ selected_card.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .container-atas {
-            background-color: #FFFAFA;
+            background-color: #FFF;
             margin-top: 10px;
             margin-bottom: 30px;
             padding: 20px;
