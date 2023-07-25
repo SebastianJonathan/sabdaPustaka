7aa1b61961a8892d05014cc0b65ceec2a44ad9d0
SebastianJonathan <banfuk.rong@gmail.com>
2023-07-25 13:07:54 +0700

    hapus awesome kit

--- header.php
+++ header.php
@@ -9,5 +9,4 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://kit.fontawesome.com/your-font-awesome-kit.js" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
