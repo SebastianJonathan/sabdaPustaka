d0e93b1cdbbd98c40950833b8f7b06528e5b79e2
mika <petra3@in-christ.net>
2023-08-02 09:44:28 +0700

    Update

--- header.php
+++ header.php
@@ -1,12 +1,27 @@
+<?php include 'config.php' ?>
 <title>Sabda Pustaka</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="http://localhost/UI/sabdaPustaka/styles3.css">
-<link rel="stylesheet" href="http://localhost/UI/sabdaPustaka/style.css">
-<link rel="stylesheet" href="http://localhost/UI/sabdaPustaka/sabdastyle.css">
-<link rel="stylesheet" href="http://localhost/UI/sabdaPustaka/getalllist.css">
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script>
+    loadCustomStyles();
+
+    function loadCustomStyles() {
+        addStyleSheet(configPath + "styles3.css");
+        addStyleSheet(configPath + "style.css");
+        addStyleSheet(configPath + "sabdastyle.css");
+        addStyleSheet(configPath + "getalllist.css");
+    }
+
+    function addStyleSheet(href) {
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = href;
+        document.head.appendChild(link);
+    }
+
+</script>
